50f02ddf127cb5ab2900d7252ed1662f20ba97c3
Alapati Santhosh Kumar <127014004@sastra.ac.in>
2026-07-25 21:23:53 +0530

    Day 5:Adding Express GET and POST routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
-
+const PORT = 5000;
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World! Express server is running.");
+  res.send("Simple Blog server is running!");
+});
+
+/* GET route: will return all blogs later */
+app.get("/api/blogs", (req, res) => {
+  res.status(200).json({
+    message: "GET route is working. Blog posts will be returned here.",
+  });
+});
+
+/* POST route: will save blogs on Day 6 */
+app.post("/api/blogs", (req, res) => {
+  const { title, author, content } = req.body;
+
+  res.status(201).json({
+    message: "POST route is working.",
+    receivedBlog: {
+      title,
+      author,
+      content,
+    },
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
